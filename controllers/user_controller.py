09cc6b82bfc144d5ab1bfbf1480755019813c298
ashlynnsutton <asutto27@students.kennesaw.edu>
2025-02-23 22:11:41 -0500

    Create user_controller.py

---
+++ controllers/user_controller.py
@@ -0,0 +1,48 @@
+import pyodbc
+
+#Have to establish connection with db via connection string
+db = f'Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:account-ant.database.windows.net,1433;Database=account-ant-employee;Uid=workerAnt;Pwd=forTheColony01!!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+connect = pyodbc.connect(db)
+
+#View all users
+def view_users():
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM Users")
+    for row in cursor.fetchall(): 
+        print(row)
+    cursor.close()
+    
+#Create new user/assign their role
+def create_user(user_id, first_name, last_name, birth_date, email, hire_date, role, status):
+    cursor = connect.cursor()
+    query = "INSERT INTO Users (user_id, first_name, last_name, birth_date, email, hire_date, role, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (user_id, first_name, last_name, birth_date, email, hire_date, role, status))
+    connect.commit()
+    cursor.close()
+
+#Delete user 
+def delete_user(user_id):
+    cursor = connect.cursor()
+    query = "DELETE FROM Users WHERE user_id = %s "
+    cursor.execute(query, (user_id))
+    connect.commit()
+    cursor.close()
+
+#Change status or activate/deactivate
+def change_status(status, user_id):
+    cursor = connect.cursor()
+    query = "UPDATE Users SET status = %s WHERE user_id = %s"
+    cursor.execute(query, (status, user_id))
+    connect.commit()
+    cursor.close()
+
+#View past passwords
+def view_expired_passwords():
+    cursor = connect.cursor()
+    cursor.execute("SELECT past_password FROM Passwords")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+
+#Have to close db connection
+connect.close()
