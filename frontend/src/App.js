6fc72df0d387af5f317025d6e7bfb515d07ccb6f
joelk22 <joelmkim382001@gmail.com>
2025-02-25 18:41:00 -0500

    Update route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup from "./components/Login/Signup";
 import ForgotPassword from "./components/Login/ForgotPassword";
+import ResetPassword from "./components/Login/ResetPassword"
+import Home from "./components/dashboard/Home"
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/dashboard" element={<Home />} />
       </Routes>
     </Router>
   );
